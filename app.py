77ae475f4f3ff88dcadb04a5e9e264571fb4e616
rootmecheo <122682476+rootmecheo@users.noreply.github.com>
2023-12-12 13:04:36 +0000

    created db_table for submitted applications

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
-from database import load_jobs_from_db
+from database import load_jobs_from_db, add_application_to_db
+
+import requests
 
 app = Flask(__name__)
 
@@ -22,6 +24,15 @@ def show_job(id):
   else:
     return "Job not found", 404  # If the job is not found, return a 404 error
 
+@app.route("/job/<id>/apply", methods=["POST"])
+def apply_to_job(id):
+  data = request.form
+  job = load_jobs_from_db(id)[0]
+
+  add_application_to_db(id, data)
+  # send email
+  # update data to database 
+  return render_template("submitted_application.html", application=data, job=job)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- database.py
+++ database.py
@@ -26,3 +26,16 @@ def load_jobs_from_db(id=None):
           jobs = [r._asdict() for r in result.all()]
           return jobs
 
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+    conn.execute(query, {
+        "job_id": job_id,
+        "full_name": data["full_name"],
+        "email": data["email"],
+        "linkedin_url": data["linkedin_url"],
+        "education": data["education"],
+        "work_experience": data["work_experience"],
+        "resume_url": data["resume_url"]
+    })
+
