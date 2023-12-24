b8416b44723b51f7630628cfc3c96655aa1a7416
rootmecheo <122682476+rootmecheo@users.noreply.github.com>
2023-12-24 19:21:15 +0000

    added post  and delete job functionality

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect
 
-from database import load_jobs_from_db, add_application_to_db
+from flask_ckeditor import CKEditor
 
+from database import add_application_to_db, load_jobs_from_db, add_job_to_db, remove_job_from_db, update_job_in_db
+
+from forms import JobForm
 
 app = Flask(__name__)
+ckeditor = CKEditor(app)
+app.secret_key = 'atlantis-global-2023'
 
 @app.route( "/")
 def main():
@@ -23,7 +28,18 @@ def show_job(id):
   else:
     return "Job not found", 404  # If the job is not found, return a 404 error
 
-@app.route("/job/<id>/apply", methods=["POST"])
+@app.route("/job/<int:id>/delete")
+def delete_job(id):
+    # Assuming load_jobs_from_db returns the job with the given id
+    job = load_jobs_from_db(id)[0]
+
+    if job:
+        remove_job_from_db(id)
+        return redirect('/')  # Redirect to the home page or any other page after deletion
+    else:
+        return ' did not successfully delete you idiot'
+
+@app.route("/job/<id>/apply")
 def apply_to_job(id):
   data = request.form
   job = load_jobs_from_db(id)[0]
@@ -33,6 +49,36 @@ def apply_to_job(id):
   # update data to database 
   return render_template("submitted_application.html", application=data, job=job)
 
+@app.route("/post_job", methods=["POST", "GET"])
+def post_job():
+  if request.method == "POST":
+    data = request.form
+    add_job_to_db(data)
+    return redirect('/')
+  else:
+    job =load_jobs_from_db()
+    return render_template('post_job.html', job=job)
+
+
+@app.route("/job/<int:id>/update", methods=["POST", "GET"])
+def update_job(id):
+    #job = load_jobs_from_db(id)[0]
+    #print(job)
+    #form = JobForm(obj=job)  # Pass the job data to the form to pre-fill the fields
+    #if request.method == "POST" and form.validate_on_submit():
+        #update_job_in_db(id, form.data)
+        #return redirect('/')
+    #return render_template("update_job.html", job=job, form=form)
+  if request.method == "POST":
+    new_data = request.form
+
+    update_job_in_db(id, new_data)
+    return redirect('/')  # Redirect to the home page 
+  else:
+    job = load_jobs_from_db(id)[0]
+  #   print(job)
+    return render_template("update_job.html", job=job)
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
   
--- database.py
+++ database.py
@@ -13,6 +13,7 @@ engine = create_engine(
         }
     } ) 
 
+# read from the database
 def load_jobs_from_db(id=None):
   if id:
       with engine.connect() as conn:
@@ -39,3 +40,45 @@ def add_application_to_db(job_id, data):
         "resume_url": data["resume_url"]
     })
 
+# add job to the database
+def add_job_to_db(data):
+    try:
+        with engine.connect() as conn:
+            query = text("INSERT INTO jobs (title, location, salary, currency, responsibilities, requirements) VALUES (:title, :location, :salary, :currency, :responsibilities, :requirements)")
+            conn.execute(query, {
+                "title": data["title"],
+                "location": data["location"],
+                "salary": data["salary"],
+                "currency": data["currency"],
+                "responsibilities": data["responsibilities"],
+                "requirements": data["requirements"]
+            })
+        return True  # Return True to indicate successful insertion
+    except Exception as e:
+        print(f"Error occurred while adding job to database: {e}")
+        return False  # Return False to indicate insertion failure
+
+# remove job from the database
+def remove_job_from_db(job_id):
+  with engine.connect() as conn:
+    query = text("DELETE FROM jobs where id = :id")
+    conn.execute(query, {"id": job_id})
+
+
+# update job in the database
+def update_job_in_db(job_id, new_data):
+  try:
+      with engine.connect() as conn:
+          query = text("""
+              UPDATE jobs
+              SET title = :title,
+                  location = :location,
+                  salary = :salary,
+                  currency = :currency,
+                  responsibilities = :responsibilities,
+                  requirements = :requirements
+              WHERE id = :id
+          """)
+          conn.execute(query, {**new_data, "id": job_id})
+  except Exception as e:
+      print(f"Error occurred while updating job: {e}")
